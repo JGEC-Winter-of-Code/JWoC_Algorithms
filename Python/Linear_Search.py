ff90dee9693c2ddd78db4928770d758bdbf77480
gr33nm0nk2802 <gr33nm0nk2802@gmail.com>
2020-01-06 12:43:03 -0800

    Created Linear_Search.py

--- Python/Linear_Search.py
+++ Python/Linear_Search.py
@@ -1,1 +1,27 @@
 
+def search(arr, x): 
+  
+    for i in range(len(arr)): 
+  
+        if arr[i] == x: 
+            return i 
+  
+    return -1
+
+print("Enter the size of the array")
+n= int(input())
+
+arr = []
+for i in range(n):
+    z=int(input())
+    arr.append(z)    
+
+print("Enter the number to search")
+x= int(input())
+
+a = search(arr,x)
+
+if a != -1:
+    print("Element is present at position",a)
+else:
+    print("Element not present")
