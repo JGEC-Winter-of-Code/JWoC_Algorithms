0cda60f47af527b030c548a4d06e7b14c1c9df98
SARTHAK RANJAN <56467031+sarthaknerd@users.noreply.github.com>
2023-03-02 15:59:41 +0530

    Bubble sort code in c++

---
+++ bubbleSort.cpp
@@ -0,0 +1,15 @@
+
+
+ #include <iostream>
+ using namespace std;
+int main(){
+  for(int i =1 ;i<n;i++){
+   
+    for(int j = 0 ;j<n-i;j++){
+      if(arr[j] > arr[j+1]){
+        swap(arr[j],arr[j+1]);
+      }
+  }
+ 
+  
+}
