56a7e8e1cd537fb2632d4c68f59b62e9a8868db3
SARTHAK RANJAN <56467031+sarthaknerd@users.noreply.github.com>
2023-03-17 12:05:22 +0530

    BubbleSort

---
+++ C++/bubbleSort.cpp
@@ -0,0 +1,13 @@
+
+
+ #include <iostream>
+//sarthak
+ int main(){
+  for(int i = 1;i<n;i++){
+   for(int j = 0; j<n-i;j++){
+    if(arr[j]>arr[j+1]){
+    swap(arr[j],arr[j+1]); 
+    }
+    }
+  
+ }
